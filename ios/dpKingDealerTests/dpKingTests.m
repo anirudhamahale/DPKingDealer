a6871e9bea5f5bca8aaae8c447da46b04325edb3
Anirudha Mahale <anirudhamahale.com>
2024-12-30 16:05:48 +0530

    Initial commit

--- ios/dpKingDealerTests/dpKingTests.m
+++ ios/dpKingDealerTests/dpKingTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface dpKingTests : XCTestCase
+@interface dpKingDealerTests : XCTestCase
 
 @end
 
-@implementation dpKingTests
+@implementation dpKingDealerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
